243e9f175335c4edd84cdaf52ffd1463c6024263
Carlos <carlosjvare@gmail.com>
2020-10-15 17:24:37 -0600

    Fixing merge problems

--- Case5/case5_api/configs/elasticConnection.ts
+++
@@ -1,7 +0,0 @@
-import {Constants} from './common'
-var elasticsearch=require('elasticsearch');
-
-var client = new elasticsearch.Client( {
-    host: Constants.ELASTIC_HOST
-});
-export default client;
