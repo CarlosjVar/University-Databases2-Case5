5defe43aa6986968b8665eb0b0f905821e005dec
Jorge Gutiérrez Cordero <jorge.guti.estudiantil@gmail.com>
2020-10-15 15:35:11 -0600

    main api folder rename

--- Case5/user_api/elasticConnection.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var elasticsearch = require('elasticsearch');
-var client = new elasticsearch.Client({
-    host: 'host.docker.internal:9200'
-});
-exports["default"] = client;
